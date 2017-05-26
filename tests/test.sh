73d677d98f5b5b814803c763114141f23e8d6594
okartal <oender.kartal@mykolab.ch>
2017-05-26 21:27:03 +0200

    Remove testdata, this has to be replaced by proper tests

--- tests/test.sh
+++
@@ -1,8 +0,0 @@
-# test help
-shannon -h
-# test without options
-shannon -o mt.bed -c MT -i metadata.csv
-# test query
-shannon -q "stage=='MI'" -c MT -i metadata.csv -o mt.stage_MI.bed
-# test with query and groupby
-shannon -q "stage=='MI'" -i metadata.csv -g patient age -c MT -o mt.stage_MI.bed
