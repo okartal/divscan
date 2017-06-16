ff2a88333909477807ef14750c8aca9c7431f91d
okartal <oender.kartal@mykolab.ch>
2017-06-16 20:38:01 +0200

    update some comments in core.py

--- shannonlib/core.py
+++ shannonlib/core.py
@@ -30,13 +30,18 @@ def divergence(sample, chrom=None, data_columns=None, outfile=None,
             print('...{:>5} % (skipped empty region)'.format(progress))
             continue
 
-        div = est.jsd_is(data)
-
-        if div.empty:
-            continue
-        
-        # import pdb; pdb.set_trace()
-
+        # within-group JSDs at each level
+        #
+        # make a list for each level: elements are lists of members
+        # map pool on each member to get data, then concat
+    #     db = data[['by4', 'by14']]
+    #     divb = est.jsd_is(db)
+    #     div = pd.concat([diva, divb], keys=['A', 'B'], names=['level_0'], axis=1)
+    #     js = div_is.xs('JSD_bit_', level='feature', axis=1)
+    #     ss = div_is.xs('sample size', level='feature', axis=1)
+    #     avg = np.average(js.values, weights=ss.values, axis=1)
+    #    div.loc[:,(slice(None), ['count_mC', 'count_C'])]
+    #     import pdb; pdb.set_trace()
         # div_subgroups = gpf.groupby('stage', metadata=sample, data=data)
         # note: use multiprocessing if you have a list of groupby objects
         # div_is = div_subgroups.apply(est.jsd_is)
@@ -45,6 +50,11 @@ def divergence(sample, chrom=None, data_columns=None, outfile=None,
         # avg = np.average(js.values, weights=ss.values, axis=1)
         # div.insert(1, 'JSD_is', avg)
 
+        div = est.jsd_is(data)
+
+        if div.empty:
+            continue
+        
         if not os.path.isfile(outfile):
             header = True
         elif os.stat(outfile).st_size == 0:
