11b7a58fb6586f4dc301c6f4085e4ecb63e11bbd
Önder Kartal <oender.kartal@mykolab.ch>
2017-07-04 03:33:48 +0200

    WIP: add function for hierarchical js_divergence
    - basic outline of algorithm, not tested yet
    - uses a dynamic programming like approach, no recursion
    - exploits theoretical insight that weights cancel along sampling
      hierarchy

--- shannonlib/estimators.py
+++ shannonlib/estimators.py
@@ -5,6 +5,7 @@
 """
 
 from functools import partial
+from itertools import compress
 from multiprocessing import Pool, cpu_count
 
 import numexpr as ne
@@ -62,14 +63,20 @@ def jsd_is(data, grouped=False):
 
         mix_entropy = shannon_entropy(data_feature.values)
         avg_entropy = np.average(shannon_entropy(counts, axis=2),
-                                 # FIXME do fillna at the end!
-                                 # ZeroDivisionError possible if all weights
-                                 # are zero!
                                  weights=data_unit.fillna(0),
                                  axis=1)
+        # FIXME: do fillna at the end? ZeroDivisionError
+        # possible if all weights become zero but that
+        # only happens if all weights ar NaN which
+        # should not happen if data is preprocessed
+        # properly
 
         jsd = np.where(np.isclose(mix_entropy, avg_entropy, atol=1.e-2),
                        .0, mix_entropy - avg_entropy)
+        # FIXME
+        # if PDF binary and ratio (here meth. level) of all counts summed is close to 0 (or 1) set JSD=0
+        # elif entropy_of_mixture close to zero set JSD=0
+        # else set JSD = entropy_of_mixture - avg_of_entropy
 
         div = data_feature
         div['JSD (bit)'] = constant.LOG2E * jsd
@@ -80,26 +87,100 @@ def jsd_is(data, grouped=False):
         return div
 
 
-def jsd_st(data):
-    """Hierarchical JS divergence.
+def js_divergence(data, meta, hierarchy=None):
+    """Returns within-group divergences along the specified sampling hierarchy.
+
+    compute the quset at each level; note that the same set can be present
+    at different levels, so do not compute div_is more than once. that
+    means, if we use a pool of workers to apply jsd_is to the *union* of
+    qusets, we need to keep track of where each set belongs to, in order
+    to make the right averaging;
+    1. make qusets (q_level1, q_level2, etc.)
+    2. make list of union of qusets (q)
+    3. make lookup table for q (essentially given by qusets)
+    columns: index(int), level1(bool), level2(bool), etc
+    if q[i] in q_level1 set level1==True, etc.
+    4. compute divIS list by applying div_is to q
+    5. compress/mask the divIS result list with the level column and average the set
+
+    FIXME: index_col must be given for meta, add index_col parameter in CLI
+    TODO: only keep loci with divergence and sample size > 1 for downstream nodes
     """
 
-    divIT = jsd_is(data)
+    div = jsd_is(data)
 
-    if divIT.empty:
-        return False
-    elif data.columns.nlevels > 2:
-        hierarchy = data.columns.names[:-2]
-        data_groups = [data.groupby(axis=1, level=hierarchy[:1 + i])
-                       for i, _ in enumerate(hierarchy)]
-        processes = min(len(hierarchy), cpu_count())
-        with Pool(processes) as pool:
-            divIS = pool.map(partial(jsd_is, grouped=True), data_groups)
+    if hierarchy is None:
+        hierarchy = []
 
-        return divIS
+    if not hierarchy or div.empty:
+        return div
+
+    title = dict()
+    quset = dict()
+
+    for i, level in enumerate(hierarchy):
+        depth = i + 1
+        eqrel = hierarchy[:depth]
+        groups = meta.groupby(eqrel).groups.values()
+        title[level] = 'JSD[{d}~{l}] (bit)'.format(d=depth, l=level)
+        quset[level] = set(tuple(sorted(s)) for s in groups if len(s) > 1)
+
+    qusetunion = list(set.union(*quset.values()))
+
+    if not qusetunion:
+        return div
     else:
-        return divIT
+        quset_data = [data[list(col)] for col in qusetunion]
+        nproc = min(len(qusetunion), cpu_count())
 
+        with Pool(processes=nproc) as pool:
+            div_qusetunion = pool.map(jsd_is, quset_data)
 
-def jsd_st_bottomup(parameter_list):
-    pass
+        for level in hierarchy:
+            # maybe precompute the mask and write a function that produces
+            # a list of the final div[i] to concatenate them with div[0]
+            mask = [1 if s in quset[level] else 0 for s in qusetunion]
+            div_quset = compress(div_qusetunion, mask)
+            if len(div_quset) == 1:
+                div[title[level]] = div_quset[0]['JSD (bit)'].values
+            else:
+                div_quset = pd.concat(div_quset, keys=None, axis=1)
+                sample_size = div_quset.xs(...).values
+                jsd = div_quset.xs(...).values
+                div[title[level]] = np.average(
+                    jsd, weights=sample_size, axis=1)
+
+    return div
+
+    #         elif depth == 1:
+    #             div_average(div_quset)
+    #         elif depth > 1:
+    #             keys = quset.keys()
+    #             dist_to_root = depth
+    #             while dist_to_root > 0:
+    #                 if dist_to_root > 1:
+    #                     groups = [list(g) for k, g in groupby(
+    #                         sorted(keys, key=lambda x: x[-2]), lambda x: x[-2])]
+    #                 elif dist_to_root == 1:
+    #                     groups = keys
+    #                 # apply a function to each group that averages or takes the DIVs
+    #                 dist_to_root -= 1
+    #                 keys =
+
+    #         # averaging depth times based on parent nodes
+    #         for k in range(depth):
+    #             for each parent node do
+    #             siblings = len() > 1
+    #             if siblings:
+    #                 d = average(d)
+
+    #         # - set parent node based on previous quset; if depth==1, parent node is root
+    #         # - group each parent node by current level; groups are child nodes
+    #         # - exclude single-element children
+    #         # - apply jsd_is to each child node
+    #         # - if only one child, take jsd_is else: average over children = jsd of parent node
+    #         # - do this 'depth' times until we get a single value per position (could also use a while loop although less explicit)
+    #         # alternatives: pd.merge; a list of pd.Series followed by pd.concat and .fillna(0)
+    #         div[title] =
+
+    # return div
